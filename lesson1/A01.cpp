bfdb798161db811af48b2d3d5e79845c6b12f5be
Metaalice <metaalice01@gmail.com>
2023-05-01 21:11:41 +0900

    lesson1_A01.cpp is added

---
+++ lesson1/A01.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/dsu>
+using namespace atcoder;
+#define rep(i, n) for (int i=0; i<n; i++)
+
+int main(void){
+  int N;
+  cin >> N;
+  cout << N*N << endl;
+
+  return 0;
+}
